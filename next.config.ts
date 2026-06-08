ee9c04f8c6a95948643a3f790722b1d25d97bb95
erxanz <172484226+erxanz@users.noreply.github.com>
2026-06-08 11:35:55 +0700

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.38:3000", "note.etres.my.id"],
+// const nextConfig: NextConfig = {
+//   allowedDevOrigins: ["192.168.1.38:3000", "thinktrackai.etres.my.id"],
+// };
+
+// export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // Matikan PWA saat tahap development agar tidak mengganggu
+});
+
+const nextConfig = {
+  // konfigurasi next.js Anda yang lain tetap di sini
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
